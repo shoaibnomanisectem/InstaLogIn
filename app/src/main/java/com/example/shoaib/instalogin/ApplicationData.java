45f094c930427c6089867590f87a51a472645667
shoaibnomanisectem <shoaibnomani@outlook.com>
2017-08-30 16:12:16 +0500

    Initial commit

---
+++ app/src/main/java/com/example/shoaib/instalogin/ApplicationData.java
@@ -0,0 +1,10 @@
+package com.example.shoaib.instalogin;
+
+/**
+ * Created by shoai on 30/08/2017.
+ */
+public class ApplicationData {
+    public static final String CLIENT_ID = "b9d2d02973ee430b9092d97e27fcb896";
+    public static final String CLIENT_SECRET = "a1b7991ed1dc43df9ba8e706d4296b23";
+    public static final String CALLBACK_URL = "instagram://connect";
+}
